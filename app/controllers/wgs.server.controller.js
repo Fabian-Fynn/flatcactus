f3f3de17d64b276181470ed9f5531b496b8d6c6c
Fabian Johannes Hoffmann <fhoffmann.mmt-b2013@fh-salzburg.ac.at>
2015-06-04 23:00:15 +0200

    feat(dashboard): display people and balance on dashboard

--- app/controllers/wgs.server.controller.js
+++ app/controllers/wgs.server.controller.js
@@ -182,7 +182,7 @@ exports.wgByPass = function(req, res, next, pass) {
 
 function isUserInWg(myArray, searchTerm) {
     for(var i = 0; i < myArray.length; i++) {
-        if (myArray[i] == searchTerm) return true;
+        if (myArray[i] === searchTerm) return true;
     }
     return false;
 }
@@ -201,7 +201,7 @@ exports.hasAuthorization = function(req, res, next) {
 };
 
 exports.isAllowedToLeave = function(req, res, next) {
-	if(req.user.wg_id != req.body._id) {
+	if(req.user.wg_id !== req.body._id) {
 		return res.status(403).send('User is not authorized');
 	}
 	next();
