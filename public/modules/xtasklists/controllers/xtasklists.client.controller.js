0de846871c410c4e2dce19271a35d48365a6bfdc
Gabriel Zangerl <gzangerl.mmt-b2013@fh-salzburg.ac.at>
2015-07-03 16:55:30 +0200

    refactor: get all users for task creation/update

--- public/modules/xtasklists/controllers/xtasklists.client.controller.js
+++ public/modules/xtasklists/controllers/xtasklists.client.controller.js
@@ -11,18 +11,7 @@ angular.module('xtasklists').controller('XtasklistsController', ['$rootScope', '
 			var obj = {};
 			var counter = 2;
 			console.log('first', $scope);
-			$scope.allUsers.forEach(function(user){
-				if(user.checked){
-					obj[user._id] = {};
-					obj[user._id].username = user.username;
-					obj[user._id].howOften = 0;
-					obj[user._id].monthly = 0;
-					obj[user._id].yearly = 0;
-					obj[user._id].crt = (user.username === $scope.first.name) ? true : false;
-					obj[user._id].turn = (user.username === $scope.first.name) ? 1 : counter++;
-					obj[user._id].isNext = (obj[user._id].turn !== 2) ? false : true;
-				}
-			});
+
 
 			var xtasklist = new Xtasklists ({
 				name: this.name,
@@ -44,6 +33,22 @@ angular.module('xtasklists').controller('XtasklistsController', ['$rootScope', '
 				$scope.error = errorResponse.data.message;
 			});
 		};
+
+		function fillUserObject(forCreation){
+			$scope.allUsers.forEach(function(user){
+				if(user.checked){
+					obj[user._id] = {};
+					obj[user._id].username = user.username;
+					obj[user._id].crt = (user.username === $scope.first.name) ? true : false;
+					obj[user._id].turn = (user.username === $scope.first.name) ? 1 : counter++;
+					obj[user._id].isNext = (obj[user._id].turn !== 2) ? false : true;
+				}
+
+				obj[user._id].howOften = 0 || $scope.xtasklist[user._id].howOften;
+				obj[user._id].monthly = 0 || $scope.xtasklist[user._id].monthly;
+				obj[user._id].yearly = 0 || $scope.xtasklist[user._id].yearly;
+			});
+		}
 
 		// Remove existing Xtasklist
 		$scope.remove = function(xtasklist) {
@@ -152,26 +157,33 @@ angular.module('xtasklists').controller('XtasklistsController', ['$rootScope', '
 			if(count === 0){ $scope.first.name = null; }
 		};
 
-		function getUsers() {
+		$scope.getUsers = function(isForCreation) {
 			$scope.removeBgClass();
-			console.log('getUsers');
 			$scope.interval = 'weekly';
+
 			$http.get('/my-share/allusers').success(function(res) {
 				$scope.allUsers = res;
+				$scope.first = { name: null };
+				$scope.totalUser = 0;
+
+				$scope.allUsers.forEach(function(user){
+					if(isForCreation){
+						user.checked = false;
+						user.first = false;
+					} else {
+						$scope.totalUser++;
+						if(user.turn === 1){
+							$scope.first.name = user.username;
+							user.first = true;
+						}
+						if($scope.xtasklist.users[user._id]) user.checked = true;
+					}
+				});
+
 			}).error(function(err){
 				$scope.error = err.data.message;
 			});
 		};
-
-		$scope.getAllForCreate = function(){
-			getUsers();
-			$scope.first = { name: null };
-
-			$scope.allUsers.forEach(function(user){
-				user.checked = false;
-				user.first = false;
-			});
-		}
 
 		$scope.removeBgClass = function(){
 			document.getElementById('container_bg').className = 'container';
