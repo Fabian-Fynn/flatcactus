c370e326d68c7524dba94d341c0ba2232e967977
Gabriel Zangerl <gzangerl.mmt-b2013@fh-salzburg.ac.at>
2015-07-05 21:18:21 +0200

    fix: leave wg

--- app/routes/wgs.server.routes.js
+++ app/routes/wgs.server.routes.js
@@ -19,7 +19,7 @@ module.exports = function(app) {
 		.post(users.requiresLogin, wgs.join);
 
 	app.route('/wgs/:wgId')
-		//.get(wgs.read)
+		.get(users.requiresLogin, wgs.read)
 		.put(users.requiresLogin, wgs.hasAuthorization, wgs.update)
 		.delete(users.requiresLogin, wgs.hasAuthorization, wgs.delete);
 
