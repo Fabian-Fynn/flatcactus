73be23c7a76f61e022a4c0e3aeab58b78df8f171
Gabriel Zangerl <gzangerl.mmt-b2013@fh-salzburg.ac.at>
2015-07-06 11:58:24 +0200

    feat: datepicker for start

--- public/config.js
+++ public/config.js
@@ -4,7 +4,7 @@
 var ApplicationConfiguration = (function() {
 	// Init module configuration options
 	var applicationModuleName = 'flatcactus';
-	var applicationModuleVendorDependencies = ['ngResource', 'ngCookies',  'ngAnimate',  'ngTouch',  'ngSanitize',  'ui.router', 'ui.bootstrap', 'ui.utils', 'ds.clock', 'ngFileUpload'];
+	var applicationModuleVendorDependencies = ['ngResource', 'ngCookies',  'ngAnimate',  'ngTouch',  'ngSanitize',  'ui.router', 'ui.bootstrap', 'ui.utils', 'ds.clock', 'ngFileUpload', 'angular-datepicker'];
 
 	// Add a new vertical module
 	var registerModule = function(moduleName, dependencies) {
