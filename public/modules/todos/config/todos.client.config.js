3f36734558209c89d8e44af5a0776001222623e1
Gabriel Zangerl <gzangerl.mmt-b2013@fh-salzburg.ac.at>
2015-07-08 16:21:34 +0200

    fix: move menu share to wgs.client.conf

--- public/modules/todos/config/todos.client.config.js
+++ public/modules/todos/config/todos.client.config.js
@@ -7,10 +7,10 @@ angular.module('todos').run(['Menus',
 		// Menus.addMenuItem('topbar', 'todos', 'todos', 'dropdown', '/todos(/create)?', false, null, 3);
 		// Menus.addSubMenuItem('topbar', 'wgs', 'ntodos', 'todos');
 		// Menus.addSubMenuItem('topbar', 'todos', 'New Todo', 'todos/create');
-		Menus.addMenuItem('topbar', 'share', 'wgs', 'dropdown', '#', false, null, 0);
-		Menus.addSubMenuItem('topbar', 'wgs', 'edit flat-share', 'my-share/edit');
-		Menus.addSubMenuItem('topbar', 'wgs', 'shoppinglist', 'shoppinglists');
-		Menus.addSubMenuItem('topbar', 'wgs', 'passphrase', 'my-share/passphrase');
-		Menus.addSubMenuItem('topbar', 'wgs', 'leave this share', 'my-share/leave');
+		// Menus.addMenuItem('topbar', 'share', 'wgs', 'dropdown', '#', false, null, 0);
+		// Menus.addSubMenuItem('topbar', 'wgs', 'edit flat-share', 'my-share/edit');
+		// Menus.addSubMenuItem('topbar', 'wgs', 'shoppinglist', 'shoppinglists');
+		// Menus.addSubMenuItem('topbar', 'wgs', 'passphrase', 'my-share/passphrase');
+		// Menus.addSubMenuItem('topbar', 'wgs', 'leave this share', 'my-share/leave');
 	}
 ]);
--- public/modules/wgs/config/wgs.client.config.js
+++ public/modules/wgs/config/wgs.client.config.js
@@ -7,5 +7,10 @@ angular.module('wgs').run(['Menus',
 		// Menus.addMenuItem('topbar', 'share', 'wgs', 'dropdown', '#');
 		// Menus.addSubMenuItem('topbar', 'wgs', 'passphrase', 'my-share/passphrase');
 		// Menus.addSubMenuItem('topbar', 'wgs', 'leave this share', 'my-share/leave');
+		Menus.addMenuItem('topbar', 'share', 'wgs', 'dropdown', '#', false, null, 0);
+		Menus.addSubMenuItem('topbar', 'wgs', 'edit flat-share', 'my-share/edit');
+		Menus.addSubMenuItem('topbar', 'wgs', 'shoppinglist', 'shoppinglists');
+		Menus.addSubMenuItem('topbar', 'wgs', 'passphrase', 'my-share/passphrase');
+		Menus.addSubMenuItem('topbar', 'wgs', 'leave this share', 'my-share/leave');
 	}
 ]);
